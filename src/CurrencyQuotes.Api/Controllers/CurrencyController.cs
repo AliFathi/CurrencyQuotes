dd849131017e25a80dc6fc5556881775b6eb74a5
Ali Fathi <alifathi1388@hotmail.com>
2024-10-11 19:38:01 +0330

    implement GetQuotes methods of ExchangeRates api. also use a specific exception.

--- src/CurrencyQuotes.Api/Controllers/CurrencyController.cs
+++ src/CurrencyQuotes.Api/Controllers/CurrencyController.cs
@@ -17,6 +17,38 @@ public class CurrencyController : ControllerBase
         return JsonContent(res);
     }
 
+    [Route("quotes/{baseSymbol:alpha:length(3)}")]
+    public async Task<IActionResult> GetSymbols(
+        [FromServices] ExchangeRatesApi api,
+        [FromRoute] string baseSymbol,
+        [FromQuery] string? symbols,
+        CancellationToken ct
+    )
+    {
+        var validSymbols = (await api.GetSymbolsAsync(ct))
+            .Symbols!.Select(s => s.Key);
+
+        if (!validSymbols.Contains(baseSymbol.ToUpper()))
+            return NotFound($"symbol {baseSymbol} not supported.");
+
+        var compareSymbols = string.IsNullOrWhiteSpace(symbols)
+            ? []
+            : symbols.Split(',');
+
+        compareSymbols = compareSymbols.Where(s => s != string.Empty).ToArray();
+        if (compareSymbols.Length > 5)
+            return BadRequest();
+
+        foreach (var symbol in compareSymbols)
+        {
+            if (!validSymbols.Contains(symbol.ToUpper()))
+                return BadRequest($"symbol {symbol} not supported.");
+        }
+
+        var res = await api.GetQuotesRawAsync(baseSymbol, compareSymbols, ct);
+        return JsonContent(res);
+    }
+
     protected ContentResult JsonContent(string content)
         => Content(content, "application/json", System.Text.Encoding.UTF8);
 }
--- src/CurrencyQuotes.Api/Utilities/ExchangeRates/ExchangeRatesApi.cs
+++ src/CurrencyQuotes.Api/Utilities/ExchangeRates/ExchangeRatesApi.cs
@@ -1,5 +1,6 @@
 ﻿using CurrencyQuotes.Utilities.ExchangeRates.Models;
 using CurrencyQuotes.Utilities.Http;
+using System.Net;
 
 namespace CurrencyQuotes.Utilities.ExchangeRates;
 
@@ -8,6 +9,12 @@ public class ExchangeRatesApi(
     ExchangeRatesOptions options
 )
 {
+    private const string _BaseUrl = "https://api.exchangeratesapi.io/v1";
+    private const string _BaseSymbol = "EUR";
+
+    /// <summary>
+    /// گرفتن همه نمادها به صورت کلاس برای استفاده در برنامه
+    /// </summary>
     public async Task<SymbolsOutput> GetSymbolsAsync(CancellationToken ct = default)
     {
         var res = await apiClient.GetAsync<SymbolsOutput>(
@@ -17,9 +24,12 @@ public class ExchangeRatesApi(
 
         return res.IsSuccessStatusCode
             ? res.Content!
-            : throw new Exception();
+            : throw new ExchangeRatesException("symbols", res.StatusCode, res.StatusMessage);
     }
 
+    /// <summary>
+    /// گرفتن همه نمادها به صورت جیسون برای فرستادن به کلاینت
+    /// </summary>
     public async Task<string> GetSymbolsRawAsync(CancellationToken ct = default)
     {
         var res = await apiClient.GetAsync(
@@ -29,14 +39,69 @@ public class ExchangeRatesApi(
 
         return res.IsSuccessStatusCode
             ? res.RawContent!
-            : throw new Exception();
+            : throw new ExchangeRatesException("symbols", res.StatusCode, res.StatusMessage);
     }
 
-    private string SymbolsUrl() =>
-        $"https://api.exchangeratesapi.io/v1/symbols?access_key={options.ApiKey}";
+    public async Task<RatesOutput> GetQuotesAsync(
+        string? baseSymbol,
+        IEnumerable<string>? compareSymbols = null,
+        CancellationToken ct = default
+    )
+    {
+        var res = await apiClient.GetAsync<RatesOutput>(
+            QuotesUrl(baseSymbol, compareSymbols),
+            ct
+        );
+
+        return res.IsSuccessStatusCode
+            ? res.Content!
+            : throw new ExchangeRatesException("quotes", res.StatusCode, res.StatusMessage);
+    }
+
+    public async Task<string> GetQuotesRawAsync(
+        string? baseSymbol,
+        IEnumerable<string>? compareSymbols = null,
+        CancellationToken ct = default
+    )
+    {
+        var res = await apiClient.GetAsync(
+            QuotesUrl(baseSymbol, compareSymbols),
+            ct
+        );
+
+        return res.IsSuccessStatusCode
+            ? res.RawContent!
+            : throw new ExchangeRatesException("quotes", res.StatusCode, res.StatusMessage);
+    }
+
+    #region Urls
+
+    private string SymbolsUrl()
+        => $"{_BaseUrl}/symbols?access_key={options.ApiKey}";
+
+    private string QuotesUrl(string? baseSymbol, IEnumerable<string>? compareSymbols)
+    {
+        var url = $"{_BaseUrl}/latest?access_key={options.ApiKey}";
+
+        if (baseSymbol != null)
+            url += $"&base={baseSymbol}";
+
+        if (compareSymbols != null)
+            url += $"&symbols={string.Join(',', compareSymbols)}";
+
+        return url;
+    }
+
+    #endregion
 }
 
 public class ExchangeRatesOptions
 {
     public required string ApiKey { get; set; }
 }
+
+public class ExchangeRatesException(
+    string name,
+    HttpStatusCode statusCode,
+    string? reasonPhrase
+) : Exception($"failed to call {name} api of ExchangeRates with code {(int)statusCode}: {reasonPhrase ?? statusCode.ToString()}");
